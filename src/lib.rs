ea4e418289ac99cf8f84d86531772cad1205fd65
yuma140902 <yuma140902@gmail.com>
2021-11-05 23:58:49 +0900

    typesモジュールを追加 (ReverieEngine)

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ pub mod gl;
 pub mod interpolation;
 pub mod shader;
 pub mod texture;
+pub mod types;
 pub mod vao;
---
+++ src/types.rs
@@ -0,0 +1,5 @@
+pub use nalgebra as na;
+
+pub type Vector3 = na::Vector3<f32>;
+pub type Matrix4 = na::Matrix4<f32>;
+pub type Point3 = na::Point3<f32>;
