3c3ed1bac3d896b5af7fdb95cb1599b7d6addac6
yuma140902 <yuma140902@gmail.com>
2021-11-10 01:34:34 +0900

    add VaoBuilder2DGui::{add_layout_rectangle, add_biggest_rectangle}

--- src/gui/vao_builder.rs
+++ src/gui/vao_builder.rs
@@ -1,8 +1,27 @@
-use crate::gui::layout::Rect;
+use crate::gui::layout::{Origin, Position, Rect};
 use crate::{texture::dynamic_texture_atlas::DynamicTextureUV, vao::VaoBuffer};
 
 pub trait VaoBuilder2DGui {
     fn add_rectangle(&mut self, texture: &DynamicTextureUV, dst: &Rect<i32, u32>);
+    fn add_layout_rectangle(
+        &mut self,
+        texture: &DynamicTextureUV,
+        parent_width: u32,
+        parent_height: u32,
+        origin: &Origin,
+        position_x: &Position<i32>,
+        position_y: &Position<i32>,
+        inner_width: u32,
+        inner_height: u32,
+    );
+    fn add_biggest_rectangle(
+        &mut self,
+        texture: &DynamicTextureUV,
+        parent_width: u32,
+        parent_height: u32,
+        inner_width: u32,
+        inner_height: u32,
+    );
 }
 
 impl VaoBuilder2DGui for VaoBuffer {
@@ -23,5 +42,43 @@ impl VaoBuilder2DGui for VaoBuffer {
             x+w, y  , 0.0,  0.0, 0.0, 1.0,  texture.end_u  , texture.begin_v,
         ];
         self.append(&mut vert);
+    }
+
+    fn add_layout_rectangle(
+        &mut self,
+        texture: &DynamicTextureUV,
+        parent_width: u32,
+        parent_height: u32,
+        origin: &Origin,
+        position_x: &Position<i32>,
+        position_y: &Position<i32>,
+        inner_width: u32,
+        inner_height: u32,
+    ) {
+        let dst = Rect::new_in_rect(
+            &Rect::new(0_i32, 0_i32, parent_width, parent_height),
+            origin,
+            position_x,
+            position_y,
+            inner_width,
+            inner_height,
+        );
+        self.add_rectangle(texture, &dst);
+    }
+
+    fn add_biggest_rectangle(
+        &mut self,
+        texture: &DynamicTextureUV,
+        parent_width: u32,
+        parent_height: u32,
+        inner_width: u32,
+        inner_height: u32,
+    ) {
+        let dst = Rect::new_biggest_in_rect(
+            &Rect::new(0_i32, 0_i32, parent_width, parent_height),
+            inner_width,
+            inner_height,
+        );
+        self.add_rectangle(texture, &dst);
     }
 }
