8baa95ba0cb9f4f881b6708bd411c264c8d1c4e0
yuma140902 <yuma140902@gmail.com>
2021-11-12 13:03:07 +0900

    add Const<C>, Dynamic

--- src/types.rs
+++ src/types.rs
@@ -3,3 +3,9 @@ pub use nalgebra as na;
 pub type Vector3 = na::Vector3<f32>;
 pub type Matrix4 = na::Matrix4<f32>;
 pub type Point3 = na::Point3<f32>;
+
+/// 定数ジェネリクスの値が定まっていることを表す型
+pub struct Const<const C: u32>;
+
+/// 定数ジェネリクスの値が定まっていないことを表す型
+pub struct Dynamic;
