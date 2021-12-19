979e3d6a7465f98bf34c86f0c92b1a8927661bcd
yuma140902 <yuma140902@gmail.com>
2021-12-20 00:04:23 +0900

    feat(sample_app): use window module

--- sample_app/src/main.rs
+++ sample_app/src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use reverie_engine as re;
+
+fn main() {
+    let config = re::window::WindowConfigBuilder::new().build();
+    let window = re::window::Window::new(&config);
+    window.show();
+    window.main_loop();
+}
