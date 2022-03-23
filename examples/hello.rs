778a7473a4e52a43c16ba7e39f342fe4b1d93e72
yuma140902 <yuma140902@gmail.com>
2022-03-23 18:23:56 +0900

    画面のクリア

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,8 +1,62 @@
+use pollster::FutureExt;
+use reverie_engine::ReverieEngine;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
+use winit::{
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 fn main() {
     let subscriber = FmtSubscriber::builder().with_max_level(Level::TRACE).finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
     println!("hello");
     tracing::info!("hello tracing");
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut engine = ReverieEngine::new(&window).block_on();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent { window_id, ref event } if window_id == window.id() => {
+            if !engine.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        engine.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        engine.resize(**new_inner_size)
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            engine.update();
+            match engine.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => engine.resize(engine.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(err) if err == wgpu::SurfaceError::Outdated => {
+                    tracing::trace!("{:?}", err);
+                }
+                Err(err) => tracing::warn!("{:?}", err),
+            }
+        }
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
+        _ => {}
+    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,123 @@
+use tracing::{debug, info, trace};
+use tracing_unwrap::{OptionExt, ResultExt};
+use winit::{event::WindowEvent, window::Window};
 
+pub struct ReverieEngine {
+    pub surface: wgpu::Surface,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub config: wgpu::SurfaceConfiguration,
+    pub size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl ReverieEngine {
+    #[tracing::instrument(skip(window))]
+    pub async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        info!("Instance: {:?}", instance);
+        let surface = unsafe { instance.create_surface(window) };
+        info!("Surface: {:?}", surface);
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap_or_log();
+        info!("Adapter: {:?}", adapter);
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .unwrap_or_log();
+        info!("Device: {:?}", device);
+        info!("Queue: {:?}", queue);
+
+        assert!(size.width > 0);
+        assert!(size.height > 0);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_preferred_format(&adapter).unwrap_or_log(),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+        info!("Config: {:?}", config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            trace!("Resize: {:?}", new_size);
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    // 戻り値がtrueならメインループはそれ以上イベントを処理しない
+    pub fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    pub fn update(&mut self) {
+        // pass
+    }
+
+    #[tracing::instrument[skip(self)]]
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
