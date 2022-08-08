c8a0f7492a8e07f9eb70c9c77b4351d8b6a0cce3
yuma140902 <yuma140902@gmail.com>
2022-08-08 09:19:23 +0900

    Textures and bind groups
    Done: https://sotrh.github.io/learn-wgpu/beginner/tutorial5-textures/

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,7 @@ pub struct ReverieEngine {
     index_buffer: wgpu::Buffer,
     num_indices: u32,
     diffuse_bind_group: wgpu::BindGroup,
+    diffuse_texture: texture::Texture
 }
 
 impl ReverieEngine {
@@ -94,54 +95,9 @@ impl ReverieEngine {
         info!("Config: {:?}", config);
 
         let diffuse_bytes = include_bytes!("./test.png");
-        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-        let diffuse_rgba = diffuse_image.to_rgba8();
         info!("Load texture");
 
-        let dimsnesions = diffuse_rgba.dimensions();
-
-        let texture_size = wgpu::Extent3d {
-            width: dimsnesions.0,
-            height: dimsnesions.1,
-            depth_or_array_layers: 1,
-        };
-        let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
-            size: texture_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-            label: Some("diffuse_texture"),
-        });
-
-        queue.write_texture(
-            wgpu::ImageCopyTexture {
-                texture: &diffuse_texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            &diffuse_rgba,
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: std::num::NonZeroU32::new(4 * dimsnesions.0),
-                rows_per_image: std::num::NonZeroU32::new(dimsnesions.1),
-            },
-            texture_size,
-        );
-        info!("Queue write texture");
-
-        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Nearest,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
+        let diffuse_texture = texture::Texture::from_bytes(&device, &queue, diffuse_bytes, Some("test.png")).unwrap_or_log();
 
         let texture_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
@@ -171,11 +127,11 @@ impl ReverieEngine {
             entries: &[
                 wgpu::BindGroupEntry {
                     binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+                    resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
                 },
                 wgpu::BindGroupEntry {
                     binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+                    resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler),
                 },
             ],
             label: Some("diffuse_bind_group"),
@@ -260,6 +216,7 @@ impl ReverieEngine {
             index_buffer,
             num_indices,
             diffuse_bind_group,
+            diffuse_texture
         }
     }
 
