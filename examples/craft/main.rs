d8a9f6743adde1af3bb476e2daa10d3bcb2ddec4
yuma140902 <yuma140902@gmail.com>
2022-08-25 14:02:03 +0900

    add process_event

--- examples/craft/main.rs
+++ examples/craft/main.rs
@@ -1,37 +1,32 @@
+use std::time::{Duration, Instant};
+
 use reverie_engine::{
     gl::{self, Gl},
     window::{Context, Window},
 };
-use winit::event_loop::ControlFlow;
 
 pub fn main() {
-    let window = Window::new();
+    let mut window = Window::new();
     let context = Context::new(&window);
     let gl = Gl::clone(&context.gl);
 
-    window.run(move |event, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    let mut start = Instant::now();
 
-        match event {
-            winit::event::Event::WindowEvent {
-                event: winit::event::WindowEvent::CloseRequested,
-                ..
-            } => {
-                *control_flow = ControlFlow::Exit;
+    while !window.process_event() {
+        let elapsed = start.elapsed();
+        if elapsed > Duration::from_millis(16) {
+            println!(
+                "{} FPS",
+                Duration::from_secs(1).as_nanos() as f64 / elapsed.as_nanos() as f64
+            );
+            context.make_current();
+            unsafe {
+                gl.ClearColor(1.0, 0.0, 1.0, 1.0);
+                gl.Clear(gl::COLOR_BUFFER_BIT);
             }
-            winit::event::Event::RedrawRequested(_) => {
-                context.make_current();
-
-                unsafe {
-                    gl.ClearColor(1.0, 0.0, 1.0, 1.0);
-                    gl.Clear(gl::COLOR_BUFFER_BIT);
-                }
-
-                context.swap_buffers();
-
-                context.make_not_current();
-            }
-            _ => {}
+            context.swap_buffers();
+            context.make_not_current();
+            start = Instant::now();
         }
-    });
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,26 +1,71 @@
-use std::ops::{Deref, DerefMut};
+use std::{
+    collections::VecDeque,
+    ops::{Deref, DerefMut},
+    sync::{Arc, Mutex},
+};
+
+use winit::platform::run_return::EventLoopExtRunReturn;
 
 use crate::gl::Gl;
 
-pub struct Window {
+pub struct EventLoop {
     event_loop: winit::event_loop::EventLoop<()>,
+    queue: Arc<Mutex<VecDeque<winit::event::Event<'static, ()>>>>,
+}
+
+impl EventLoop {
+    fn new() -> Self {
+        let event_loop = winit::event_loop::EventLoop::new();
+        let queue = Arc::new(Mutex::new(VecDeque::new()));
+        Self { event_loop, queue }
+    }
+
+    fn process_event(&mut self) -> bool {
+        let queue = Arc::clone(&self.queue);
+        let mut exit = false;
+        self.event_loop.run_return(|event, _, control_flow| {
+            let (push_event, continue_polling) = match event {
+                winit::event::Event::WindowEvent {
+                    event: winit::event::WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    exit = true;
+                    (false, false)
+                }
+                winit::event::Event::MainEventsCleared => (false, false),
+                winit::event::Event::RedrawRequested(_) => (false, false),
+                _ => (true, true),
+            };
+
+            if push_event {
+                let mut q = queue.lock().unwrap();
+                q.push_back(event.to_static().unwrap());
+            }
+            if !continue_polling {
+                *control_flow = winit::event_loop::ControlFlow::Exit;
+            }
+        });
+        return exit;
+    }
+}
+
+pub struct Window {
+    event_loop: EventLoop,
     window: winit::window::Window,
 }
 
 impl Window {
     pub fn new() -> Self {
-        let event_loop = winit::event_loop::EventLoop::new();
-        let window = winit::window::WindowBuilder::new().build(&event_loop).unwrap();
+        let event_loop = EventLoop::new();
+        let window = winit::window::WindowBuilder::new()
+            .build(&event_loop.event_loop)
+            .unwrap();
 
         Self { event_loop, window }
     }
 
-    pub fn run<F>(self, mut handler: F)
-    where
-        F: 'static + FnMut(winit::event::Event<'_, ()>, &mut winit::event_loop::ControlFlow) -> (),
-    {
-        self.event_loop
-            .run(move |event, _, control_flow| handler(event, control_flow));
+    pub fn process_event(&mut self) -> bool {
+        self.event_loop.process_event()
     }
 }
 
