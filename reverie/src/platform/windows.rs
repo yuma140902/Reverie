3f11dfd1c03e11eeb9cd537b20f379a832ee7b45
yuma140902 <yuma140902@gmail.com>
2022-09-21 12:19:55 +0900

    タスクマネージャーが起動したときにエラーコード0でパニックするのを修正

--- reverie/src/platform/windows.rs
+++ reverie/src/platform/windows.rs
@@ -2,5 +2,11 @@ pub use windows::core::Error;
 use windows::Win32::UI::WindowsAndMessaging::SetCursorPos;
 
 pub fn set_cursor_pos(x: i32, y: i32) -> Result<(), Error> {
-    unsafe { SetCursorPos(x, y) }.ok()
+    let result = unsafe { SetCursorPos(x, y) }.ok();
+    if let Err(ref err) = result {
+        if err.code().is_ok() {
+            return Ok(());
+        }
+    }
+    result
 }
