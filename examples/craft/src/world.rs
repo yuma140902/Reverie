896f5535932bc0d71bec3ae6c3ae46eac25df872
yuma140902 <yuma140902@gmail.com>
2022-09-22 21:42:33 +0900

    add collision AABBs to world

--- examples/craft/src/world.rs
+++ examples/craft/src/world.rs
@@ -1,4 +1,5 @@
 use nalgebra::Point3;
+use parry3d::bounding_volume::AABB;
 use re::gl::Gl;
 use re::CuboidTextures;
 use re::Vao;
@@ -20,6 +21,12 @@ fn pos_to_index(x: u32, y: u32, z: u32) -> usize {
     (16 * 16 * y + 16 * z + x) as usize
 }
 
+fn get_block_aabb(x: u32, y: u32, z: u32) -> AABB {
+    let min = Point3::new(x as f32, y as f32, z as f32);
+    let max = min + BLOCK_SIZE;
+    AABB::new(min, max)
+}
+
 impl World {
     pub fn new() -> World {
         World {
@@ -29,6 +36,21 @@ impl World {
 
     pub fn set_block(&mut self, x: u32, y: u32, z: u32) {
         self.blocks[pos_to_index(x, y, z)] = true;
+    }
+
+    pub fn collision_aabbs(&self) -> Vec<AABB> {
+        let mut v = Vec::new();
+        for x in 0..16 {
+            for y in 0..16 {
+                for z in 0..16 {
+                    let index = pos_to_index(x, y, z);
+                    if self.blocks[index] {
+                        v.push(get_block_aabb(x, y, z));
+                    }
+                }
+            }
+        }
+        v
     }
 
     pub fn generate_vertex_obj<'a>(
@@ -42,7 +64,7 @@ impl World {
         for x in 0..16 {
             for y in 0..16 {
                 for z in 0..16 {
-                    let index = (16 * 16 * y + 16 * z + x) as usize;
+                    let index = pos_to_index(x, y, z);
                     let block = self.blocks[index];
                     if !block {
                         continue;
