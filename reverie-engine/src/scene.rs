b22eb35835cc5ac39378f2722b9f8f66a99d039e
yuma14 <yuma140902@gmail.com>
2025-12-21 15:44:37 +0900

    feat: Scene の構造を新しくする
    シーンが Mesh, Matrial, Model, GameObject などから構成されるようにする。

--- reverie-engine/src/scene.rs
+++ reverie-engine/src/scene.rs
@@ -1,16 +1,21 @@
 //! シーンに関するモジュール
 
-use crate::{render::RenderingResource, scene::frame::Frame};
+use crate::{render::RenderingResource, scene::frame::Frame, texture::TextureId};
 
 mod components;
 pub mod frame;
 
 pub use components::{
-    colored::ColoredComponent, sprite::SpriteComponent, transform::TransformComponent,
+    colored::ColoredComponent, model::ModelComponent, sprite::SpriteComponent,
+    transform::TransformComponent,
 };
 
-#[derive(Default)]
-pub struct Scene {}
+#[derive(Debug, Default)]
+pub struct Scene {
+    pub meshes: Registry<MeshKey, Mesh>,
+    pub materials: Registry<MaterialKey, Material>,
+    pub game_objects: Registry<GameObjectKey, GameObject>,
+}
 
 impl Scene {
     pub const fn setup(&mut self, _resource: &RenderingResource<'_>) {}
@@ -34,8 +39,70 @@ impl Scene {
     }
 }
 
-impl std::fmt::Debug for Scene {
+pub struct Registry<K: slotmap::Key, V> {
+    map: slotmap::DenseSlotMap<K, V>,
+}
+
+impl<K: slotmap::Key, V> Default for Registry<K, V> {
+    fn default() -> Self {
+        Self {
+            map: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vertex {
+    pub position: [f32; 3],
+    pub uv: [f32; 2],
+    pub normal: [f32; 3],
+}
+
+pub struct Mesh {
+    pub name: String,
+    pub vertices: Vec<Vertex>,
+    pub indices: Vec<u32>,
+    pub vertex_count: u32,
+    pub index_count: u32,
+}
+
+slotmap::new_key_type! { pub struct MeshKey; }
+
+#[derive(Debug)]
+pub struct Material {
+    pub texture: TextureId,
+}
+
+slotmap::new_key_type! { pub struct MaterialKey; }
+
+#[derive(Debug)]
+pub struct GameObject {
+    pub name: String,
+    pub model_component: ModelComponent,
+}
+
+slotmap::new_key_type! { pub struct GameObjectKey; }
+
+impl<K: slotmap::Key, V> std::fmt::Debug for Registry<K, V> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Scene").finish()
+        let Self { map } = self;
+        f.debug_struct("Registry").field("len", &map.len()).finish()
+    }
+}
+
+impl std::fmt::Debug for Mesh {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self {
+            name,
+            vertices: _,
+            indices: _,
+            vertex_count,
+            index_count,
+        } = self;
+        f.debug_struct("Mesh")
+            .field("name", name)
+            .field("vertex_count", vertex_count)
+            .field("index_count", index_count)
+            .finish()
     }
 }
